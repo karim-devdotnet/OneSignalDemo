5d9cc3b5cce046c591dd3ab169de94c35735396c
karimabd84 <abdelkarim.esti@gmail.com>
2017-05-29 10:58:53 +0200

    Projektdateien hinzufügen.

---
+++ OneSignal.Demo/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneSignal.Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SendPushNotification();
+        }
+
+        private static void SendPushNotification()
+        {
+            var request = WebRequest.Create("https://onesignal.com/api/v1/notifications") as HttpWebRequest;
+
+            request.KeepAlive = true;
+            request.Method = "POST";
+            request.ContentType = "application/json; charset=utf-8";
+
+            request.Headers.Add("authorization", "Basic NGEwMGZmMjItY2NkNy0xMWUzLTk5ZDUtMDAwYzI5NDBlNjJj");
+
+            byte[] byteArray = Encoding.UTF8.GetBytes("{"
+                                                    + "\"app_id\": \"9423ab70-f216-4a77-a364-cdf74f40e4fb\","
+                                                    + "\"contents\": {\"en\": \"English Message\"},"
+                                                    + "\"headings\": {\"en\": \"Test...\"},"
+                                                    + "\"data\": {\"foo\": \"bar\",\"your\": \"custom metadata\"},"
+                                                    + "\"include_player_ids\": [\"17135056-a51d-4b45-bc7c-731b4b3a79eb\"]}");
+
+            string responseContent = null;
+
+            try
+            {
+                using (var writer = request.GetRequestStream())
+                {
+                    writer.Write(byteArray, 0, byteArray.Length);
+                }
+
+                using (var response = request.GetResponse() as HttpWebResponse)
+                {
+                    using (var reader = new StreamReader(response.GetResponseStream()))
+                    {
+                        responseContent = reader.ReadToEnd();
+                    }
+                }
+            }
+            catch (WebException ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex.Message);
+                System.Diagnostics.Debug.WriteLine(new StreamReader(ex.Response.GetResponseStream()).ReadToEnd());
+            }
+
+            System.Diagnostics.Debug.WriteLine(responseContent);
+        }
+    }
+}
